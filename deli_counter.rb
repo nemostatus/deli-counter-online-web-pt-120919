e370990b5f36eaf0549c99d26bd07a960eb42e10
Matthew Corralejo <mattcorralejo1@gmail.com>
2019-12-15 12:19:44 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -24,8 +24,8 @@ take_a_number(katz_deli, "Fyvish")
   if line.length == 0 # could say: "if deli.empty?"
     puts"There is nobody waiting to be served!"
   else
-    puts "Currently serving #{line[0]}." # could say: "Currently serving #{line.first}."
-    line.shift # this works in the IDE but no on repl.it
+    puts "Currently serving #{line[0]}." 
+    line.shift
   end
 end
 
