c18dfc97e05525b469138b747ea633535e9c8d10
Matthew Corralejo <mattcorralejo1@gmail.com>
2019-12-15 11:25:43 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,23 @@
-# Write your code here.
+def line(array)
+  if array.size == 0
+    puts "The line is currently empty."
+  else
+    index_array = []
+  array.each_with_index {|name,index|
+    index_array.push "#{index+1}. #{name}"}
+    puts "The line is currently: #{index_array.join(' ')}"
+    end
+    end
+    line(["Logan","Avi","Spencer"])
+    
+  
+def take_a_number(line,name)
+  if name == line[-1]
+  puts "Welcome, #{name} You are number #{line.length} in line."
+  else 
+  nil
+  end
+  end
+  take_a_number(["Ada"],"Ada")
+      
+   
